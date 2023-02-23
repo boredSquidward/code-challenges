b9e4c1664be00bf484a70ede8d380fc0f5721312
boredSquidward <120290053+boredSquidward@users.noreply.github.com>
2023-02-24 00:44:38 +0330

    fibonacci solved

--- dist/fibonacciNumber.js
+++ dist/fibonacciNumber.js
@@ -1,13 +1,12 @@
 "use strict";
-function climbStairs(n) {
+function fib(n) {
     let fibo = [0, 1];
     if (n === 0)
         return fibo[0];
     if (n === 1)
         return fibo[1];
-    for (let i = 2; i <= n + 1; i++) {
+    for (let i = 2; i <= n; i++) {
         fibo.push(fibo[i - 1] + fibo[i - 2]);
     }
-    return fibo[n + 1];
+    return fibo[n];
 }
-console.log(climbStairs(4));
