5bf8df19ec14089a7bbf7292d102f73cea7b24c4
boredSquidward <120290053+boredSquidward@users.noreply.github.com>
2023-02-24 00:29:52 +0330

    climbStairs solved

---
+++ src/climbStairs.ts
@@ -0,0 +1,14 @@
+function climbStairs(n: number): number {
+  let fibo = [0, 1];
+
+  if (n === 0) return fibo[0];
+  if (n === 1) return fibo[1];
+
+  for (let i = 2; i <= n; i++) {
+    fibo.push(fibo[i - 1] + fibo[i - 2]);
+  }
+
+  return fibo[n];
+}
+
+console.log(climbStairs(4));
