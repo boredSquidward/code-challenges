9bc8c37d192dcdf773532c72780036445e1dbc51
boredSquidward <120290053+boredSquidward@users.noreply.github.com>
2023-07-22 12:09:49 -0400

    working on intToRoman

--- intToRoman.js
+++ intToRoman.js
@@ -1,5 +1,25 @@
 function convertToRoman(num) {
-  const roman = {}
+  const roman = {
+    1000: "M",
+    500: "D",
+    100: "C",
+    50: "L",
+    10: "X",
+    5: "V",
+    1: "I",
+  };
+
+  const str = num.toString();
+  const arr = str.split("").reverse();
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = +arr[i] * Math.pow(10, i);
+  }
+
+  console.log(arr);
 }
 
-//
+//XII
+//MMXXIII
+
+convertToRoman(1819);
